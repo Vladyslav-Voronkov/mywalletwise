783a91c4b0940c5fe58fb26d2ef22e8203e92101
Vladyslav-Voronkov <vladyslavvoronkov@gmail.com>
2023-06-08 19:10:27 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div className='flex justify-center items-center w-full h-screen bg-gray-900'>
+      <div>
+        <h1 className='text-gray-500 font-bold text-2xl'>Wallet Wise</h1>
+        <p className='text-center text-white'>June 2023</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
